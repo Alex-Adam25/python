9118a705ab5d855b386b17e02c070d27bb92759a
Alex-Adam25 <68447567+Alex-Adam25@users.noreply.github.com>
2021-01-08 15:35:38 +0100

    update

---
+++ scrapy/rki.bat
@@ -0,0 +1,3 @@
+call C:\Users\alexa\anaconda3\Scripts\activate.bat scrapy
+cd C:\Users\alexa\Documents\GitHub\python\scrapy
+python rki.py
---
+++ scrapy/rki.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import urllib.request 
+site= "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Impfquotenmonitoring.html"
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+req = Request(site, headers=hdr)
+html_page = urlopen(req)
+soup = BeautifulSoup(html_page, "lxml")
+soup.find("a",{"class":"more downloadLink InternalLink"})
+dl_link=r"https://rki.de"+soup.find("a",{"class":"more downloadLink InternalLink"})['href']
+date=soup.find("a",{"class":"more downloadLink InternalLink"})['title'].split('(')[1].split(',')[0]
+print(dl_link)
+print(date)
+opener = urllib.request.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+urllib.request.install_opener(opener)
+urllib.request.urlretrieve(dl_link, f"{date}.xlsx")
